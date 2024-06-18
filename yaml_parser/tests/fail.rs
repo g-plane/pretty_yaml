4618ac134a61c3e32b7162536a1790bbd401c79d
Pig Fang <g-plane@hotmail.com>
2024-06-18 14:50:35 +0800

    test(parser): add "fail" tests

---
+++ yaml_parser/tests/fail.rs
@@ -0,0 +1,31 @@
+use insta::{assert_snapshot, glob, Settings};
+use std::{fs, path::Path};
+
+#[test]
+fn fmt_snapshot() {
+    glob!("fail/*.yaml", |path| {
+        let input = fs::read_to_string(path).unwrap();
+        let err_msg = if let Err(err) = yaml_parser::parse(&input) {
+            err.to_string()
+        } else {
+            panic!("expected '{}' to fail", path.display());
+        };
+
+        build_settings(path).bind(|| {
+            let name = path.file_stem().unwrap().to_str().unwrap();
+            assert_snapshot!(name, err_msg);
+        });
+    });
+}
+
+fn build_settings(path: &Path) -> Settings {
+    let mut settings = Settings::clone_current();
+    settings.set_snapshot_path(path.parent().unwrap());
+    settings.remove_snapshot_suffix();
+    settings.set_prepend_module_to_snapshot(false);
+    settings.remove_input_file();
+    settings.set_omit_expression(true);
+    settings.remove_input_file();
+    settings.remove_info();
+    settings
+}
